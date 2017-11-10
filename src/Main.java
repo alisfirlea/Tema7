97e82a6b8029972e93090e67f5e5956c220e7016
Sfirlea Alina <alinasfirlea23@yahoo.com>
2017-11-10 17:53:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        double pret =50.00;
+        double procentTva=0.19;
+        double valoareTva=pret * procentTva;
+        System.out.println("Tva-ul este de "+ valoareTva + " lei");
+
+    }
+}
